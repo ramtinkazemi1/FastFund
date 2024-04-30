16fbea15a505e32f506b83e302c2f1559234debe
Ramtin Kazemi <ramkazemi@yahoo.com>
2024-04-30 01:14:03 -0700

    separated the routes

--- FastFund.py
+++ FastFund.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask
+from sqlalchemy import create_engine, Column, String, Numeric
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+engine = create_engine(os.getenv("DATABASE_URI"))
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
+Session = sessionmaker(bind=engine)
+FastFund_db = Session()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    phone_number = Column(String(20), primary_key=True)
+    email = Column(String(100), unique=True)
+    password_hash = Column(String(255), nullable=False)
+    full_name = Column(String(100))
+    balance = Column(Numeric(15, 2), default=0.00)
+
+
+from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    Base.metadata.create_all(engine)
+    app.run(port=5000)
+
+
